72c7884fc1f5c74ac20b5436a0bb41a8f384b52b
DanielVafidis <daniel.vafidis@gmail.com>
2020-05-01 20:21:39 +0200

    fixed CORS error

--- src/cointainers/Projects.js
+++ src/cointainers/Projects.js
@@ -12,17 +12,9 @@ const Projects = () => {
     const [data, setData] = useState({});
     const [ready, setReady] = useState(false);
 
-    var myHeaders = new Headers();
-    myHeaders.append('access-control-allow-origin', '*');
-    var myInit = {
-        method: 'GET',
-        headers: myHeaders,
-        mode: 'cors',
-        cache: 'default'
-    };
 
     useEffect(() => {
-        fetch(URL, myInit)
+        fetch(URL)
             .then((results) => results.json())
             .then((resp) => {
                 setData(resp);
